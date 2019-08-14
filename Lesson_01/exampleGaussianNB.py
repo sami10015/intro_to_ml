fcd57d0ce69569b73bf33dfb9667167073a0ec97
sellougani <sellougani@flashpoint-intel.com>
2019-08-14 14:35:09 -0400

    Added comments for accuracy logic

--- Lesson_01/exampleGaussianNB.py
+++ Lesson_01/exampleGaussianNB.py
@@ -6,4 +6,9 @@ Y = np.array([1, 1, 1, 2, 2, 2]) # Labels, either class 1 or class 2
 
 clf = GaussianNB()
 clf.fit(X, Y) # This is where you give the training data, and the classification algorithm learns
-print(clf.predict([[-0.8, -1]])) # Predict what the label is for this point
+prediction = clf.predict([[-0.8, -1]]) # Predict what the label is for this point
+print(prediction)
+
+# Here is a way that you can predict how accurate your classification is
+# from sklearn.metrics import accuracy_score
+# accuracy = accuracy_score(sample_of_labels, prediction)
